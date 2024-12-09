749a39b784f0f709a2ec2b39f6393745cebbd576
0bitr1x <flyshoot954@gmail.com>
2024-12-09 14:54:51 +0200

    init

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,39 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val list1 = listOf(1, 2, 3, 4, 5)
+    val list2 = listOf(4, 5, 6, 7, 8)
+
+    val synonyms = mapOf(
+        "happy" to "joyful",
+        "sad" to "unhappy",
+        "fast" to "quick",
+        "slow" to "sluggish"
+    )
+    val word = "happy"
+
+    println("Четные элементы списка:")
+    for (number in numbers) {
+        if (number % 2 == 0) {
+            println(number)
+        }
     }
+
+    val set1 = list1.toSet()
+    val set2 = list2.toSet()
+    val intersection = set1.intersect(set2)
+
+    println("Общие числа: $intersection")
+    println("Количество общих чисел: ${intersection.size}")
+
+
+
+    if (synonyms.containsKey(word)) {
+        println("Синоним для '$word': ${synonyms[word]}")
+    } else {
+        println("Синоним для '$word' не найден.")
+    }
+
 }
